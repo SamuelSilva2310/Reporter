ff03d63d35fdc629ea86283e7cf2a9aa99f37020
Samuel Silva <samuelsilva2310@gmail.com>
2022-01-20 14:57:10 +0000

    Updated run Script

--- ansible/reporter/run.sh
+++ ansible/reporter/run.sh
@@ -3,4 +3,6 @@ set -e
 if ! ps ax | grep -q "[m]ain.py"; then
 source /root/ansible_test/reporter/venv/bin/activate
 nohup python3 /root/ansible_test/reporter/main.py -p $1 -o $2  <&- &> my.admin.log.file &
+else
+echo "running"
 fi
